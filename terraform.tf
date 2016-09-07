6447e7b9d13fbb523f77f4c0d844c6f9c384e3ed
Stefan Magnus Landrø <stefan.landro@gmail.com>
2016-09-07 09:40:39 +0200

    Adding ssh keys and connect to droplets

--- terraform.tf
+++ terraform.tf
@@ -11,4 +11,11 @@ resource "digitalocean_droplet" "web" {
   name = "web-server-${count.index}"
   region = "ams2"
   size = "512mb"
+  ssh_keys = ["${digitalocean_ssh_key.ssh.id}"]
+}
+
+# Create SSH key that can be connected to droplets
+resource "digitalocean_ssh_key" "ssh" {
+  name = "Terraform Example"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDMRib4BZcrv3hS8agJA0BOGlU/5P8lW5h5HW5elxEWuzElRjCr0ReEVAceVBm50rGc/rqf40zfaNGMGXe3c3u6r4ZMLNbq+xjh7aeC0zeAvAEQmzH0ErW+aLVda3CA2G2vKt1zMGv4Vu5IoH4gj0fZUiv2OKgheuPe/icH8Mc5j/rqbVEXR2HZHcjDIwf1BcBeDIMg2Nsh+bvCTh3KGLdEkEg9bhK0RNj/s4qEoi8INA1dLCK7TLTzuHtW7Ntdvg1lTHEzhHh2U4coGJi5VPLfPnwLpNr04sC89KNUzVIuxrp8FxfgZbLR4harX3Mp8C0q98s/C/BoN/QxotX5dPJH stefan.landro@gmail.com"
 }
