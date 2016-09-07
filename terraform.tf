30a7e062a22b70a73980519996d3bdbf98bfcd6d
Stefan Magnus Landrø <stefan.landro@gmail.com>
2016-09-07 10:12:04 +0200

    Adding DNS for ssh management

--- terraform.tf
+++ terraform.tf
@@ -24,3 +24,24 @@ resource "digitalocean_ssh_key" "ssh" {
   name = "Terraform Example"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDMRib4BZcrv3hS8agJA0BOGlU/5P8lW5h5HW5elxEWuzElRjCr0ReEVAceVBm50rGc/rqf40zfaNGMGXe3c3u6r4ZMLNbq+xjh7aeC0zeAvAEQmzH0ErW+aLVda3CA2G2vKt1zMGv4Vu5IoH4gj0fZUiv2OKgheuPe/icH8Mc5j/rqbVEXR2HZHcjDIwf1BcBeDIMg2Nsh+bvCTh3KGLdEkEg9bhK0RNj/s4qEoi8INA1dLCK7TLTzuHtW7Ntdvg1lTHEzhHh2U4coGJi5VPLfPnwLpNr04sC89KNUzVIuxrp8FxfgZbLR4harX3Mp8C0q98s/C/BoN/QxotX5dPJH stefan.landro@gmail.com"
 }
+
+# Domain name
+variable "digital_ocean_domain_name" {
+  default = "do.landro.io"
+}
+
+# Looked up manually in aws route 53 console
+# Consider using aws_route53_zone resource instead
+variable "dns_zone_id" {
+  default = "Z2X1UBSEPFNQNM"
+}
+
+# Create DNS records in order to manage servers
+resource "aws_route53_record" "ssh" {
+  count = "${var.number_of_servers}"
+  zone_id = "${var.dns_zone_id}"
+  name = "ssh${count.index}.${var.digital_ocean_domain_name}"
+  type = "A"
+  ttl = "60"
+  records = ["${element(digitalocean_droplet.web.*.ipv4_address, count.index)}"]
+}
